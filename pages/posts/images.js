cb203520b5abed808dfc08a62cc5e5defaaeaa19
luthfiazi <luthfiazizi11@gmail.com>
2021-10-19 15:39:11 +0700

    integration tailwind in nextjs

---
+++ pages/posts/images.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+const profilePict = () =>(
+    <Image 
+        src= "/images/profile.jpg"
+        height={144}
+        width={144}
+        alt='Luthfi azizi'
+    />
+)
